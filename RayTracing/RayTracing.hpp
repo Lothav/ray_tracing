459baaecd67a8e717d2f47ad464c72c3aba781d3
Luiz0tavio <me@luizotav.io>
2018-06-27 21:11:19 -0300

    fillColorMap() improved -> loop through plane doing good

--- RayTracing/RayTracing.hpp
+++ RayTracing/RayTracing.hpp
@@ -29,20 +29,33 @@ namespace RayTracing
 
             auto camera_eye                  = camera->getEye();
             auto camera_center               = camera->getCenter();
+            auto camera_up                   = camera->getUp();
 
             auto projection_plane_normal     = glm::normalize(camera_eye - camera_center);
             float projection_plane_d         = -(glm::dot(camera_center, projection_plane_normal));
             auto projection_plane_equation   = glm::vec4(projection_plane_normal, projection_plane_d);
 
-            int projection_plane_len         = glm::length(camera_eye - camera_center) * glm::tan(glm::radians(camera->getFov())) * 2;
+            float projection_plane_len       = glm::length(camera_eye - camera_center) * glm::tan(glm::radians(camera->getFov()));
 
-            for (int i = 0; i < projection_plane_len; i++) {
-                for (int j = 0; j < projection_plane_len; j--) {
+            auto x = camera_center.x - projection_plane_len;
+            auto y = camera_center.y - projection_plane_len;
+            auto z = -((projection_plane_equation.x * x) + (projection_plane_equation.y * y) + projection_plane_d) / (projection_plane_equation.z);
 
-                    auto u = static_cast<float>(i) / static_cast<float>(projection_plane_len);
-                    auto v = static_cast<float>(j) / static_cast<float>(projection_plane_len);
+            auto lower_left_corner = glm::vec3(x, y, z);
 
-                    auto* ray = new Ray(camera->getEye(), lower_left_corner + u*horizontal + v*vertical);
+            for (int i = 0; i < projection_plane_len * 2; i++) {
+                this->color_map.push_back({});
+                for (int j = 0; j < projection_plane_len * 2; j++) {
+
+                    auto u = static_cast<float>(i) / (projection_plane_len * 2);
+                    auto v = static_cast<float>(j) / (projection_plane_len * 2);
+
+                    auto direction = lower_left_corner + u*glm::vec3(projection_plane_len*2, 0, 0) + v*glm::vec3(0, projection_plane_len*2, 0);
+                    direction.z = -((projection_plane_equation.x * direction.x) + (projection_plane_equation.y * direction.y) + projection_plane_d) / (projection_plane_equation.z);
+
+                    std::cout << "(" << direction.x << ", " << direction.y << ", " << direction.z << ")" << std::endl;
+
+                    auto* ray = new Ray(camera->getEye(), direction);
 
                     uint color = 0;
                     for (auto& object : data_->getObjects()) {
