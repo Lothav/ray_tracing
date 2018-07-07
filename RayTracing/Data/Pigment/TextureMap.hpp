ca10ab9ca9c43a3d59869e58b73c6cc72ef199e9
Luiz0tavio <me@luizotav.io>
2018-07-07 07:08:16 -0300

    TextureMap load texture file

--- RayTracing/Data/Pigment/TextureMap.hpp
+++ RayTracing/Data/Pigment/TextureMap.hpp
@@ -6,9 +6,12 @@
 #define RAY_TRACING_TEXTUREMAP_HPP
 
 #include "Pigment.hpp"
+#include "../../../Util/Util.hpp"
 #include <string>
 #include <glm/vec4.hpp>
 #include <utility>
+#include <fstream>
+#include <iostream>
 
 namespace RayTracing
 {
@@ -17,15 +20,28 @@ namespace RayTracing
 
     private:
 
-        std::string texture_path_;
+        std::string                 texture_path_;
 
-        glm::vec4   map_vec_1_;
-        glm::vec4   map_vec_2_;
+        glm::vec4                   map_vec_1_;
+        glm::vec4                   map_vec_2_;
+
+        std::vector<std::string>    data_;
 
     public:
 
         TextureMap(std::string texture_path, glm::vec4 map_vec_1, glm::vec4 map_vec_2)
-                : texture_path_(std::move(texture_path)), map_vec_1_(map_vec_1), map_vec_2_(map_vec_2) {}
+                : texture_path_(std::move(texture_path)), map_vec_1_(map_vec_1), map_vec_2_(map_vec_2)
+        {
+            std::fstream fs;
+            fs.open(texture_path_, std::fstream::in);
+
+            if (!fs.is_open()) {
+                std::cerr << "Cant open file "  << texture_path_ << std::endl;
+            }
+
+            this->data_ = Util::ReadStream(fs);
+            fs.close();
+        }
     };
 }
 
