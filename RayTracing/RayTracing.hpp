f7ec7903fd2b3fbbc8e3576a5d51856726b90b6a
Luiz0tavio <me@luizotav.io>
2018-07-07 21:53:36 -0300

    getFinalColor() recursive method

--- RayTracing/RayTracing.hpp
+++ RayTracing/RayTracing.hpp
@@ -31,9 +31,6 @@ namespace RayTracing
         void fillColorMap()
         {
             auto* camera  = data_->getCamera();
-            auto lights   = data_->getLights();
-            auto pigments = data_->getPigment();
-            auto finishes = data_->getFinishes();
 
             auto camera_eye              = camera->getEye();
             auto camera_center           = camera->getCenter();
@@ -62,54 +59,7 @@ namespace RayTracing
                     direction.z = -((camera_center.x * direction.x) + (camera_center.y * direction.y) + projection_plane_d) / (camera_center.z);
 
                     auto* d_ray = new Ray(camera->getEye(), direction);
-
-                    glm::vec3 min_intersection = {};
-                    Object*   near_object = nullptr;
-
-                    glm::vec3 final_color(.0f);
-                    if (getIntersection(d_ray, near_object, min_intersection, nullptr)) {
-
-                        auto ambient_light = lights[0];
-
-                        auto finish  = finishes[near_object->getFinishIndex()];
-                        auto light_c = finish->getLightCoefficients();
-
-                        auto pigment = pigments[near_object->getPigmentIndex()]->getColor(min_intersection) ;
-
-                        glm::vec3 ambient = ambient_light->getColor() * light_c.x;
-                        glm::vec3 diffuse(0.f);
-                        float specular = 0.f;
-
-                        for (int k = 1; k < lights.size(); ++k) {
-
-                            auto l_pos = lights[k]->getPos();
-                            auto l_color = lights[k]->getColor();
-                            auto* l_ray = new Ray(min_intersection, l_pos - min_intersection);
-
-                            glm::vec3 l_min_intersection = {};
-                            Object*   l_near_object = nullptr;
-
-                            auto found_i = getIntersection(l_ray, l_near_object, l_min_intersection, near_object);
-
-                            auto dist_l  = glm::length(l_pos - min_intersection);
-                            auto dist_i  = glm::length(l_min_intersection  - min_intersection);
-
-                            if (!found_i || dist_l < dist_i) {
-
-                                auto N = near_object->getNormal(min_intersection);
-                                auto L = glm::normalize(l_pos - min_intersection);
-                                auto V = glm::normalize(camera->getEye() - min_intersection);
-                                auto R = glm::reflect(-L, N);
-
-                                diffuse  += light_c.y * l_color * std::max(glm::dot(N, L), 0.0f) * glm::vec3(1.0f);
-                                specular += light_c.z * pow(std::max(glm::dot(R, V), 0.0f), light_c.w) * 1.0f; // 1.0f = alpha
-                            }
-                            delete l_ray;
-                        }
-
-                        final_color = pigment*(ambient + diffuse) + specular;
-                    }
-                    delete d_ray;
+                    auto final_color = getFinalColor(d_ray);
 
                     this->color_map_[this->color_map_.size()-1].push_back(final_color);
                 }
@@ -117,6 +67,71 @@ namespace RayTracing
         }
 
     private:
+
+        glm::vec3 getFinalColor(Ray* d_ray)
+        {
+            auto* camera  = data_->getCamera();
+            auto lights   = data_->getLights();
+            auto pigments = data_->getPigment();
+            auto finishes = data_->getFinishes();
+
+            glm::vec3 min_intersection = {};
+            Object*   near_object = nullptr;
+
+            glm::vec3 final_color(.0f);
+            if (getIntersection(d_ray, near_object, min_intersection, nullptr)) {
+
+                auto ambient_light = lights[0];
+
+                auto finish  = finishes[near_object->getFinishIndex()];
+                auto light_c = finish->getLightCoefficients();
+                auto model_c = finish->getModelCoefficients();
+                auto N = near_object->getNormal(min_intersection);
+                auto V = glm::normalize(camera->getEye() - min_intersection);
+
+                auto pigment = pigments[near_object->getPigmentIndex()]->getColor(min_intersection) ;
+
+                glm::vec3 ambient = ambient_light->getColor() * light_c.x;
+                glm::vec3 diffuse(0.f);
+                float specular = 0.f;
+
+                for (int k = 1; k < lights.size(); ++k) {
+
+                    auto l_pos   = lights[k]->getPos();
+                    auto l_color = lights[k]->getColor();
+                    auto* l_ray  = new Ray(min_intersection, l_pos - min_intersection);
+
+                    glm::vec3 l_min_intersection = {};
+                    Object*   l_near_object = nullptr;
+
+                    auto found_i = getIntersection(l_ray, l_near_object, l_min_intersection, near_object);
+
+                    auto dist_l  = glm::length(l_pos - min_intersection);
+                    auto dist_i  = glm::length(l_min_intersection - min_intersection);
+
+                    if (!found_i || dist_l < dist_i) {
+
+                        auto L = glm::normalize(l_pos - min_intersection);
+                        auto R = glm::reflect(-L, N);
+
+                        diffuse  += light_c.y * l_color * std::max(glm::dot(N, L), 0.0f) * glm::vec3(1.0f);
+                        specular += light_c.z * pow(std::max(glm::dot(R, V), 0.0f), light_c.w) * 1.0f; // 1.0f = alpha
+                    }
+
+                    delete l_ray;
+                }
+
+                final_color = pigment*(ambient + diffuse) + specular;
+
+                if (model_c.x > 0.f) {
+                    auto R = glm::reflect(-V, N);
+                    final_color += model_c.x * getFinalColor(new Ray(min_intersection, min_intersection+R));
+                }
+            }
+            delete d_ray;
+
+            return final_color;
+        }
 
         bool getIntersection(Ray* ray, Object*& near_object, glm::vec3& min_intersection, Object* from_object)
         {
