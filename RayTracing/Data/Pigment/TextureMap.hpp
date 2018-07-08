fb69d80328803c80566c57a4c66ef61b942576be
Luiz0tavio <me@luizotav.io>
2018-07-08 17:02:30 -0300

    texture map fix

--- RayTracing/Data/Pigment/TextureMap.hpp
+++ RayTracing/Data/Pigment/TextureMap.hpp
@@ -21,12 +21,15 @@ namespace RayTracing
 
     private:
 
-        std::string                 texture_path_;
+        std::string                             texture_path_;
 
-        glm::vec4                   map_vec_1_;
-        glm::vec4                   map_vec_2_;
+        glm::vec4                               map_vec_1_;
+        glm::vec4                               map_vec_2_;
 
-        std::vector<std::string>    data_;
+        std::vector<std::vector<glm::vec3>>     data_;
+        uint                                    nx;
+        uint                                    ny;
+        uint                                    range;
 
     public:
 
@@ -40,15 +43,58 @@ namespace RayTracing
                 std::cerr << "Cant open file "  << texture_path_ << std::endl;
             }
 
-            this->data_ = Util::ReadStream(fs);
+            auto file_data = Util::ReadStream(fs);
+
+            int i = 0;
+            for (; i < file_data.size(); i++) {
+
+                if (file_data[i][0] == '#') continue;
+                if (file_data[i][0] == 'P') continue;
+
+                auto size = Util::GetFloats(file_data[i]);
+                if (size.size() != 2) std::cerr << "Error format ppm file" << std::endl;
+                nx = static_cast<uint>(size[0]);
+                ny = static_cast<uint>(size[1]);
+
+                i++;
+                auto range_f = Util::GetFloats(file_data[i]);
+                if (range_f.size() != 1) std::cerr << "Error format ppm file" << std::endl;
+                range = static_cast<uint>(range_f[0]);
+                i++;
+
+                break;
+            }
+
+            for (uint j = 0; j < nx; ++j) {
+                data_.resize(j+1);
+                for (uint k = 0; k < ny; ++k) {
+                    auto base_index = static_cast<int>(3*j + 3*nx*k + 2 + i);
+                    if (base_index % 3 != 0) {
+                        if ((base_index+1) % 3  == 0){
+                            base_index += 1;
+                        } else if ((base_index-1) % 3  == 0) {
+                            base_index -= 1;
+                        } else {
+                            std::cerr << "Error base index texture mapping: " << base_index << std::endl;
+                        }
+                    }
+
+                    auto grb = glm::vec3(
+                        std::stoi(file_data[base_index - 2]) / 255.f,
+                        std::stoi(file_data[base_index - 1]) / 255.f,
+                        std::stoi(file_data[base_index - 0]) / 255.f
+                    );
+
+                    data_[j].resize(k+1);
+                    data_[j][k] = grb;
+                }
+            }
+
             fs.close();
         }
 
         glm::vec3 getColor(glm::vec3 intersection, double u, double v) override
         {
-            int nx = 467;
-            int ny = 350;
-
             double i = (u * nx);
             double j = (1 - v)*ny - 0.001;
 
@@ -57,22 +103,7 @@ namespace RayTracing
             if(i > nx - 1) i = nx - 1;
             if(j > ny - 1) j = ny - 1;
 
-            auto base_index = static_cast<int>(3*i + 3*nx*j + 2 + 4);
-            if (base_index % 3 != 0) {
-                if ((base_index+1) % 3  == 0){
-                    base_index += 1;
-                } else if ((base_index-1) % 3  == 0) {
-                    base_index -= 1;
-                } else {
-                    std::cerr << "Error base index texture mapping: " << base_index << std::endl;
-                }
-            }
-
-            float r = std::stoi(data_[base_index - 2]) / 255.0f;
-            float g = std::stoi(data_[base_index - 1]) / 255.0f;
-            float b = std::stoi(data_[base_index - 0]) / 255.0f;
-
-            return glm::vec3(r, g, b);
+            return data_[i][j];
         }
     };
 }
